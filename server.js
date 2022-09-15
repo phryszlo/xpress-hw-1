971381a0041edf1d8f2ae3f341143888c85ac178
phryszlo <phryszlo@gmail.com>
2022-09-15 17:49:08 -0400

    tip calculator express app created

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/:name', (req, res) => {
   res.send(`Hello, ${req.params.name}`)
 })
 
+app.get('/tip/:total/:tip', (req, res) => {
+  res.send(`$${(req.params.tip / req.params.total * 100).toFixed(2)}`);
+})
+
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
 })
