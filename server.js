320c6cc7598873583b6ec73928d1fc04620f0a9a
phryszlo <phryszlo@gmail.com>
2022-09-15 18:04:58 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = new express();
 const PORT = process.env.PORT || 3000;
 
+const magic8Responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
 app.get('/', (req, res) => {
   res.send('Hello, robot.')
 })
@@ -12,7 +14,21 @@ app.get('/:name', (req, res) => {
 })
 
 app.get('/tip/:total/:tip', (req, res) => {
-  res.send(`$${(req.params.tip / req.params.total * 100).toFixed(2)}`);
+  res.send(`
+    <div style='display: flex; align-items: center; justify-content: center; width: 100vw; height: 100vh;color: white; background-color: black;'>
+    <h2 style='text-align: center;'>
+    $${(req.params.tip / req.params.total * 100).toFixed(2)}
+    </h2>
+    </div>
+  `);
+})
+
+app.get('/magic/:q', (req, res) => {
+  let i = Math.floor(Math.random() * magic8Responses.length);
+  res.send(`
+  <h1>${req.params.q}</h1>
+  <h1>${magic8Responses[i]}</h1>
+  `);
 })
 
 app.listen(PORT, () => {
